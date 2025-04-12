4fb9cefdd072bf72a6b0cb7fd5ae4d8b04430266
Aleksandr Zaugolnov <zausasha@gmail.com>
2025-04-12 12:11:26 +0300

    изменена логики проекта добавлен класс

---
+++ src/main/java/org/example/Box.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Box {
+    private int size;
+
+
+    public Box(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("111111");
+        System.out.println("10001");
+    }
 
+    public static void doSomething(){
+        System.out.println(1);
     }
 }
