c3c127eb6ff8227825879f904d3ecea889102063
Aleksandr Zaugolnov <zausasha@gmail.com>
2025-04-12 12:22:17 +0300

    изменили логику main и doSomething

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("---");
         System.out.println("Hello World!");
         System.out.println("10001");
     }
 
     public static void doSomething(){
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(1);
+        System.out.println(1);
     }
 }
